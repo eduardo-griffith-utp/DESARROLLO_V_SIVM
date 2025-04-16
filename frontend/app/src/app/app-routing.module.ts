32bdda0035bbf7527f6553bb6329ac36819f975b
ipomares21 <111026257+ipomares21@users.noreply.github.com>
2025-04-16 18:08:36 -0500

    añadiendo carpetes de Share

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  {
+    path: 'components',
+    loadChildren: () => import('./shared/components/components.module').then( m => m.ComponentsPageModule)
   }
+
 ];
 
 @NgModule({
