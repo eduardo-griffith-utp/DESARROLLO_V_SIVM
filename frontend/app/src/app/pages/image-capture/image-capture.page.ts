4418da23c31dedf88af73d41d0447fc3df07cbab
S0rek1X5 <91152730+S0rek1X5@users.noreply.github.com>
2025-04-16 22:47:38 -0500

    Teminando de armar la carpeta pages

---
+++ frontend/app/src/app/pages/image-capture/image-capture.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-capture',
+  templateUrl: './image-capture.page.html',
+  styleUrls: ['./image-capture.page.scss'],
+})
+export class ImageCapturePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/app/src/app/pages/recognition-results/recognition-results.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recognition-results',
+  templateUrl: './recognition-results.page.html',
+  styleUrls: ['./recognition-results.page.scss'],
+})
+export class RecognitionResultsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
