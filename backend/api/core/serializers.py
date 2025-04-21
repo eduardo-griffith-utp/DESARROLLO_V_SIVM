e7149fde9b88dde4077160b03b80edb104b6234b
Alexander Moreno <alexanderfranco233@gmail.com>
2025-04-21 13:52:13 -0500

    Estructura del proyecto back-end - Equipo Back End

---
+++ backend/api/core/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Example
+class ExampleSerializer(serializers.ModelSerializer):
+ class Meta:
+    model = Example
+    fields = ['id', 'name', 'description', 'created_at']
