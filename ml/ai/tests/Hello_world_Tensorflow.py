e756304eb5b2b1826c482417f1d196c2f24d7f17
Jose Ariano <143540999+Tannerflea05@users.noreply.github.com>
2025-04-27 19:19:25 -0500

    Update Hello_world_Tensorflow.py

--- ml/ai/tests/Hello_world_Tensorflow.py
+++ ml/ai/tests/Hello_world_Tensorflow.py
@@ -1,8 +1,9 @@
-import tensorflow as tf
-
-# Create a Tensor.
-hello = tf.constant("hello world")
-print(hello)
-
-# To access a Tensor value, call numpy().
-print(hello.numpy())
+import tensorflow as  tf
+import tkinter as tk
+hello_world = tf.constant("Hello World")
+hw_value = hello_world.numpy().decode('utf-8')
+window = tk.Tk()
+window.title("Impresion de Hello World con TensorFlow")
+label = tk.Label(window, text=f"{hw_value}")
+label.pack(padx=20,pady=20)
+window.mainloop()
