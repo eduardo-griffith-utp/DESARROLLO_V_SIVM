5183f2c64395b54efe75e48883fa44541c6f61d0
Ricardo17711 <ricado.abrego1@utp.ac.pa>
2025-05-07 20:18:42 -0500

    actualizando_estructura

--- database/script/foreignkeys.sql
+++ database/script/foreignkeys.sql
@@ -1,5 +1,5 @@
 -- Llaves foráneas corregidas
-ALTER TABLE MediaContent 
+/*ALTER TABLE MediaContent 
 ADD CONSTRAINT fk_mediacontent_session
 FOREIGN KEY (session_id) REFERENCES Session(id_session);
 
@@ -18,3 +18,13 @@ FOREIGN KEY (media_id) REFERENCES MediaContent(id_media);
 ALTER TABLE ItemTags 
 ADD CONSTRAINT fk_itemtags_items
 FOREIGN KEY (items_id) REFERENCES Items(id_items);
+*/
+
+ALTER TABLE MediaContent  
+ADD CONSTRAINT fk_mediacontent_item
+FOREIGN KEY (item_id) REFERENCES Item(id_item);
+
+ALTER TABLE item  
+ADD CONSTRAINT fk_mediacontent_item_tag
+FOREIGN KEY (item_tag_id) REFERENCES item_tag(id_tag);
+
