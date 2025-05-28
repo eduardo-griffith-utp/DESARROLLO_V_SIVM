98a44cfae8d598360cb53bfc5f8fa33b8f490a7a
HandMeTheCode <cristian.andrade@utp.ac.pa>
2025-05-28 12:18:12 -0500

    Modificaciones al item-detail.page de .html, .scss y .ts

--- frontend/app/src/app/pages/item-details/item-details.page.ts
+++ frontend/app/src/app/pages/item-details/item-details.page.ts
@@ -8,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemDetailsPage implements OnInit {
 
+  itemTitle: string = '';
+  itemImage: string = '';
+  itemDescription: string = '';
+
   constructor() { }
 
   ngOnInit() {
-  }
+    // Simulación de datos cargados desde un JSON
+    const data = {
+      title: 'Perro',
+      image: 'assets/images/dog.webp',
+      description: 'El perro (Canis familiaris o Canis lupus familiaris, dependiendo de si se lo considera una especie o una subespecie del lobo)...'
+    };
 
+    this.itemTitle = data.title;
+    this.itemImage = data.image;
+    this.itemDescription = data.description;
+  }
 }
+
+
