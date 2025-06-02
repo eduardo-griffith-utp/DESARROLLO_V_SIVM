797f477b8e43e7aac520b59e962b439345866cd5
CESAR <eliascastillo2509@gmail.com>
2025-06-02 16:47:01 -0500

    Update de los scripts iniciales

--- database/script/script-data/AnalysisResult.sql
+++ database/script/script-data/AnalysisResult.sql
@@ -1,8 +1,8 @@
 -- CRUD para la tabla AnalysisResult
 
 -- CREATE
-INSERT INTO AnalysisResult (id_analysis_result, item_id, analysis_id, detected_labes, date_analysis, status)
-VALUES (1, 1, 1, 'canguro', GETDATE(), 'VALIDO');
+INSERT INTO AnalysisResult (item_id, analysis_id, detected_labes, date_analysis, status)
+VALUES (1, 1, 'canguro', GETDATE(), 'VALIDO');
 
 -- READ
 SELECT * FROM AnalysisResult;
--- database/script/script-data/ItemTag.sql
+++ database/script/script-data/ItemTag.sql
@@ -1,35 +1,35 @@
 -- CRUD para la tabla ItemTag
 
 -- CREATE
-INSERT INTO ItemTag (id_tag, tag_name)
+INSERT INTO ItemTag (tag_name)
 VALUES 
-(1, 'CANGURO'),
-(2, 'CRISTO RENDENTOR'),
-(3, 'CUBIERTO'),
-(4, 'DELFIN'),
-(5, 'ELEFANTE'),
-(6, 'ESTATUA DE LA LIBERTAD'),
-(7, 'JIRAFA'),
-(8, 'KOALA'),
-(9, 'LAPTOP'),
-(10, 'LENTES'),
-(11, 'LEON'),
-(12, 'LIBRO'),
-(13, 'LLAVE'),
-(14, 'MOUSE'),
-(15, 'OSO POLAR'),
-(16, 'PANDA'),
-(17, 'PARAGUA'),
-(18, 'PEZ PAYASO'),
-(19, 'PUENTE CENTENARIO'),
-(20, 'RINOCERONTE'),
-(21, 'ROD CAREW ESTADIO'),
-(22, 'SMARTPHONE'),
-(23, 'TAZA'),
-(24, 'TIGRE'),
-(25, 'TORRE EIFFEL'),
-(26, 'VENTILADOR'),
-(27, 'VOLCAN BARU');
+('CANGURO'),
+('CRISTO RENDENTOR'),
+('CUBIERTO'),
+('DELFIN'),
+('ELEFANTE'),
+('ESTATUA DE LA LIBERTAD'),
+('JIRAFA'),
+('KOALA'),
+('LAPTOP'),
+('LENTES'),
+('LEON'),
+('LIBRO'),
+('LLAVE'),
+('MOUSE'),
+('OSO POLAR'),
+('PANDA'),
+('PARAGUA'),
+('PEZ PAYASO'),
+('PUENTE CENTENARIO'),
+('RINOCERONTE'),
+('ROD CAREW ESTADIO'),
+('SMARTPHONE'),
+('TAZA'),
+('TIGRE'),
+('TORRE EIFFEL'),
+('VENTILADOR'),
+('VOLCAN BARU');
 
 -- READ
 SELECT * FROM ItemTag;
