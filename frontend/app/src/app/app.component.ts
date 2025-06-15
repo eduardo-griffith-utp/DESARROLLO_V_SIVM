c59ac6663a01be9acbc907f6eca8cfe21578f50b
HandMeTheCode <cristian.andrade@utp.ac.pa>
2025-06-15 01:46:06 -0500

    se modifica app.component.ts en donde... se importa TranslateService, se crea y agrega la funcion  define idioma por defecto y funcion cambiar idioma activo

--- frontend/app/src/app/app.component.ts
+++ frontend/app/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { SplashScreen } from '@capacitor/splash-screen';
 import { Platform } from '@ionic/angular';
 import { IonProgressBar } from '@ionic/angular/standalone';
+import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -15,7 +16,10 @@ export class AppComponent {
   splash =true;
 
   showSplash = true;
-  constructor(){
+  constructor(private translateService:TranslateService){
+    this.translateService.setDefaultLang('es');
+    this.translateService.use(localStorage.getItem('lang') || 'es');
+
     setTimeout(() => {
       this.showSplash = false;
     }, 5000);
@@ -27,8 +31,6 @@ export class AppComponent {
     }, 50);
   }
 
-  
-  
   /*constructor(private platform: Platform) {
      this.initializeApp();
   }
