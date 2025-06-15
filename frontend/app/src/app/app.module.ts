d308b7d383b5462568c5a5c0718eaa0104e31f46
HandMeTheCode <cristian.andrade@utp.ac.pa>
2025-06-15 01:41:53 -0500

    se modifica app.module.ts en donde... se importa httpclient, HttpClientModule, TranslateModule, TranslateLoader, TranslateHttpLoader

--- frontend/app/src/app/app.module.ts
+++ frontend/app/src/app/app.module.ts
@@ -7,13 +7,30 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ServicesService } from './core/services/services.service';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [IonicModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [ServicesService,{  provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
+    TranslateModule.forRoot(
+      {
+      loader:{
+        provide:TranslateLoader,
+        useFactory:HttpLoaderFactory,
+        deps:[HttpClient]
+      }
+    }
+    )
+  ],
+  providers: [HttpClient, ServicesService,{  provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
