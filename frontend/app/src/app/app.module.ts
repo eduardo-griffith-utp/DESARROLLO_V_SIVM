39b37bc366de4d5570a140f64b7956acf7d7a1e3
HandMeTheCode <cristian.andrade@utp.ac.pa>
2025-07-16 16:47:26 -0500

    imported audioplayermodule and infocardmodule to enable usage across pages

--- frontend/app/src/app/app.module.ts
+++ frontend/app/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ServicesService } from './core/services/services.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { AudioPlayerModule } from './components/audio-player/audio-player.module';
+import { InfoCardModule } from './components/info-card/info-card.module';
 
 
 export function HttpLoaderFactory(http: HttpClient) {
@@ -19,7 +21,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AudioPlayerModule, InfoCardModule, HttpClientModule,
     TranslateModule.forRoot(
       {
       loader:{
